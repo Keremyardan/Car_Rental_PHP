8e5aa5fcdcdc2c47fd0f06778eaf6177796e4fcb
keremyardan <keremyrdn@hotmail.com>
2024-12-09 14:05:02 +0300

    project has started

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+session_start();
+include('includes/config.php');
+error_reporting(0);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Car Rental Portal</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+</head>
+<body>
+    
+</body>
+</html>
