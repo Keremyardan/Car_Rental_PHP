231e37e6acf0f34b0f06dfd37e067476de4f6ae5
keremyardan <keremyrdn@hotmail.com>
2024-12-24 13:30:42 +0300

    sidebar.php has been fixed

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,6 +5,7 @@
         <li><a href="update-password.php">Update Password</a></li>
         <li><a href="my-booking.php">My Booking</a></li>
         <li><a href="post-testimonial.php">Post a Testimonial</a></li>
+        <li><a href="my-testimonials.php">My Testimonials</a></li>
         <li><a href="logout.php">Sign Out!</a></li>
 
     </ul>
